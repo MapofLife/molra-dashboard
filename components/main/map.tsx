b08a0b4a66e397c0d9fd55d4fac6af7ca938af75
Chris Manciero <cmanciero@gmail.com>
2024-09-05 10:00:31 -0400

    remove message from console

--- components/main/map.tsx
+++ components/main/map.tsx
@@ -351,8 +351,6 @@ function OpenLayersMap({
     const fetchAndDisplaySiteAndLayers = async () => {
       try {
         mapRef.current?.classList.add('spinner');
-        
-        console.log('loading map')
         const sites = await fetchSites();
         const layers = await fetchLayers(selectedSubsite);
         const selectedSite = sites.find((site: any) => site.site_id === selectedSubsite);
